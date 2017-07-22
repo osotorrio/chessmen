dba72c5df4fc37418357bdd219ef4ebfbf085070
osotorrio <osotorrio@hotmail.com>
2017-07-22 01:55:03 +0100

    Moving project to a differenct solution

--- src/Chessmen.Core/Contracts/IChessmen.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Chessmen.Core.Contracts
-{
-    public interface IChessmen
-    {
-        IEnumerable<string> GetPseudoMovesFrom(ISquare square);
-    }
-}
--- src/Chessmen.Core/Enums/ColorEnum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Chessmen.Core.Enums
-{
-    public enum Color
-    {
-        White = 1,
-        Black = -1
-    }
-}
